5c7d76b8f9843c885030e7c5955073afc399b7d4
Marcus Hamill <marcusehamill@gmail.com>
2018-10-17 14:48:10 -0500

    add comments for routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
     # get '/messages' => 'messages#index'
+    #**************
     #these two routes below seems wrong because they seems like post methods.. creating new info in the db
-    # get '/messages' => 'messages#create' 
-    # get '/messages/:text' => 'messages#create'
-    #seems like it should look more like these below
+    get '/messages' => 'messages#create' 
+    get '/messages/:text' => 'messages#create'
+    #**************
+    #seems like it should look more like these below but then again these don't work in chrome just in insomnia.
     #post "/messages" => "messages#create"
     #post '/messages/:text' => 'messages#create'
     post 'messages/' => 'messages#create'
